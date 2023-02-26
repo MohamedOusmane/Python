3ac063ec57a4bc62b53153366b5692698d36b554
MohamedOusmane <ousibmohamed07@gmail.com>
2023-02-26 10:13:08 +0100

    Exercise 1 & 2

---
+++ Exercice 1.py
@@ -0,0 +1,12 @@
+#Calcule du prix TTC
+prixHorsTTC=float(input("saisir le prix hors ttc="))
+TTC=(prixHorsTTC*0.2)+prixHorsTTC
+print("Le prix TTC est:",TTC)
+
+#Calcule de la moyenne de deux variable
+x=float(input("Donner la variable x:"))
+y=float(input("Donner la variable x:"))
+moyenne=(x+y)/2
+print("la moyenne est:",moyenne)
+
+
